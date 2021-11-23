790272f149dea2c52aacb3dad104d1b574125523
MacReiben <macreiben@gmail.com>
2021-11-23 23:09:43 +0100

    first table added.

--- src/components/FuelHelperMain.vue
+++ src/components/FuelHelperMain.vue
@@ -1,10 +1,48 @@
 <template>
     <div class="container">
         <div class="row">
-            <label for="LaptimesValue" class="col-sm-2 col-form-label">laptimes (sec)</label>
+            <div class="col-sm-2 header">laptimes (sec)</div>
             <div class="col"><input type="text" class="form-control editable" id="LaptimesValue"></div>
-            <label for="TankCapacity" class="col-sm-2 col-form-label">Tank capacity (liter)</label>
+             <div class="col-sm-2 header">Tank capacity (liter)</div>
             <div class="col"><input type="text" class="form-control editable" id="TankCapacity"></div>
         </div>
     </div>
+    <br/>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2">&nbsp;</div>
+            <div class="col-sm-2 header">Time (sec)</div>
+            <div class="col-sm-2 header">Description</div>
+            <div class="col-sm-2 header">Lap count</div>
+            <div class="col-sm-2 header">Consumption (liters)</div>
+            <div class="col-sm-2 header">Service (liters)</div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2 header">Race total time (sec)</div>
+            <div class="col-sm-2"><input type="text" class="form-control editable" id="TotalRaceTime"></div>
+            <div class="col-sm-2 data">01h30:00</div>
+            <div class="col-sm-2 data">TODO<!-- Estimated lap time that will be run --></div> 
+            <div class="col-sm-2 data">TODO<!-- Estimated consumption --></div>
+            <div class="col-sm-2 data">TODO<!-- Estimated service --></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2 header">Race half time (sec)</div>
+            <div class="col-sm-2 data">TODO<!-- computed from total race time --></div>
+            <div class="col-sm-2 data">00h45:00</div>
+            <div class="col-sm-2 data">TODO<!-- Estimated lap time that will be run --></div> 
+            <div class="col-sm-2 data">TODO<!-- Estimated consumption --></div>
+            <div class="col-sm-2 data">TODO<!-- Estimated service --></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2 header">Race 60% (sec)</div>
+            <div class="col-sm-2 data">TODO<!-- computed from total race time --></div>
+            <div class="col-sm-2 data">00h54:00</div>
+            <div class="col-sm-2 data">TODO<!-- Estimated lap time that will be run --></div> 
+            <div class="col-sm-2 data">TODO<!-- Estimated consumption --></div>
+            <div class="col-sm-2 data">TODO<!-- Estimated service --></div>
+        </div>
+    </div>
 </template>
+<style scoped>
+@import '../assets/styles/computationgrid.css';
+</style>
